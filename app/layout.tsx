b2b220401aa6abc08cd10fb7a9423abfeae076d2
EdenPark <edengoforit@gmail.com>
2023-04-05 08:24:27 +1200

    logo and navbarstyling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,23 @@
+import Navbar from "./components/navbar/Navbar"
 import "./globals.css"
 import { Nunito } from "next/font/google"
 
 export const metadata = {
   title: "Air BnB",
-  description: "Air Bnb Created By Eden",
+  description: "Air Bnb Created By Eden"
 }
 
 const font = Nunito({
-  subsets: ["latin"],
+  subsets: ["latin"]
 })
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
 export default function Home() {
-  return (
-    <div>Hello Air Bnb</div>
-  )
+  return <div className="text-rose-500">Hello Air Bnb</div>
 }
