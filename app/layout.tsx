e870d0d230e89249e4de87253d24e5a070354b14
EdenPark <edengoforit@gmail.com>
2023-04-13 20:55:07 +1200

    fix of next.js hydration issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,28 @@
-import Navbar from "./components/navbar/Navbar"
-import "./globals.css"
-import { Nunito } from "next/font/google"
+import ClientOnly from './components/ClientOnly';
+import Modal from './components/modals/Modal';
+import Navbar from './components/navbar/Navbar';
+import './globals.css';
+import { Nunito } from 'next/font/google';
 
 export const metadata = {
-  title: "Air BnB",
-  description: "Air Bnb Created By Eden"
-}
+  title: 'Air BnB',
+  description: 'Air Bnb Created By Eden'
+};
 
 const font = Nunito({
-  subsets: ["latin"]
-})
+  subsets: ['latin']
+});
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          <Navbar />
+          <Modal />
+        </ClientOnly>
         {children}
       </body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div className="text-rose-500">Hello Air Bnb</div>
+  return null;
 }
