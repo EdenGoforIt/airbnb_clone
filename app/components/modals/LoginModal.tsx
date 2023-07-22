905fb4530b8f025c13f864bb06cf253446260082
Eden Park <edengoforit@gmail.com>
2023-07-22 21:22:25 +1200

    log in github added

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -100,7 +100,7 @@ const LoginModal = () => {
         outline
         label="Continue with Github"
         icon={AiFillGithub}
-        onClick={() => { }}
+        onClick={() => signIn('github')}
       />
       <div className="
       text-neutral-500 text-center mt-4 font-light">
