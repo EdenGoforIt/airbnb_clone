0419404da00f530d1e5d9145c0c991360d9f045e
Eden Park <edengoforit@gmail.com>
2023-07-23 17:01:53 +1200

    proxy domain added

--- next.config.js
+++ next.config.js
@@ -5,7 +5,9 @@ const nextConfig = {
   },
   images: {
     domains: [
-      "avatars.githubusercontent.com"
+      "avatars.githubusercontent.com",
+      'avatars.githubusercontent.com',
+      'lh3.googleusercontent.com'
     ]
   }
 }
