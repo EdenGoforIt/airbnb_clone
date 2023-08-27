d93d279f8001b377b00e367d0eada80a77c37805
Eden Park <edengoforit@gmail.com>
2023-08-28 10:32:01 +1200

    categories implemented

---
+++ app/components/navbar/category/Categories.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { categories } from '@/app/data-provider/categories';
+import { usePathname, useSearchParams } from 'next/navigation';
+import Container from '../../Container';
+import CategoryBox from './CategoryBox';
+
+const Categories = () => {
+  const params = useSearchParams();
+  const category = params?.get('category');
+  console.log('🚀 ~ file: Categories.tsx:11 ~ Categories ~ category:', category);
+  const pathName = usePathname();
+  const isMainPage = pathName === '/';
+
+  if (!isMainPage) {
+    return null;
+  }
+
+  return (
+    <Container>
+      <div
+        className="
+        pt-4
+        flex
+        flex-row
+        items-center
+        justify-between
+        overflow-x-auto"
+      >
+        {categories.map((item) => (
+          <CategoryBox key={item.label} icon={item.icon} label={item.label} selected={category === item.label} />
+        ))}
+      </div>
+    </Container>
+  );
+};
+
+export default Categories;
