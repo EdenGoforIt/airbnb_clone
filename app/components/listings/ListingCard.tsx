108564ff622d0f65084fcc06744042b37c3bf8cf
Eden Park <edengoforit@gmail.com>
2023-11-01 07:52:55 +1300

    heart button added

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -1,8 +1,13 @@
+'use client';
+
 import useCountries from '@/app/hooks/useCountry';
 import { SafeUser } from '@/app/types';
 import { Listing, Reservation } from '@prisma/client';
+import { format } from 'date-fns';
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import React, { useCallback, useMemo } from 'react';
+import HeartButton from '../HeartButton';
 
 interface ListingCardProps {
   data: Listing;
@@ -40,14 +45,40 @@ const ListingCard: React.FC<ListingCardProps> = ({
     [onAction, actionId, disabled]
   );
 
-  
-
   const price = useMemo(() => {
     if (reservation) {
       return reservation.totlaPrice;
     }
     return data.price;
   }, [reservation, data.price]);
-  return <div>Listing card</div>;
+
+  const reservationDate = useMemo(() => {
+    if (!reservation) {
+      return null;
+    }
+
+    const start = new Date(reservation.startDate);
+    const end = new Date(reservation.endDate);
+
+    return `${format(start, 'PP')} - ${format(end, 'PP')}`;
+  }, [reservation]);
+
+  return (
+    <div onClick={() => router.push(`/listing/${data.id}`)} className="col span-1 cursor-pointer group">
+      <div className="flex flex-col gap-2 w-full">
+        <div className="aspect-square w-full relative overflow-hidden rounded-xl">
+          <Image
+            fill
+            alt="Listing"
+            src={data.imageSrc}
+            className="object-cover h-full w-full group-hover:scale-110 transition"
+          />
+          <div className="absolute top-3 right-3">
+            <HeartButton listingId={data.id} currentUser={currentUser} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default ListingCard;
