0135e2dbcae9cf98d03f775197ed3b8eb2c99618
Eden Park <edengoforit@gmail.com>
2023-11-01 20:48:54 +1300

    finished listing card

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -7,6 +7,7 @@ import { format } from 'date-fns';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import React, { useCallback, useMemo } from 'react';
+import Button from '../Button';
 import HeartButton from '../HeartButton';
 
 interface ListingCardProps {
@@ -77,6 +78,15 @@ const ListingCard: React.FC<ListingCardProps> = ({
             <HeartButton listingId={data.id} currentUser={currentUser} />
           </div>
         </div>
+        <div className="font-semibold text-lg">
+          {location?.region} {location?.label}
+        </div>
+        <div className="font-light text-neutral-500">{reservationDate || data.category}</div>
+        <div className="flex flex-row items-center gap-1">
+          <div className="font-semibold">$ {price}</div>
+          {!reservation && <div className="font-light">night</div>}
+        </div>
+        {onAction && actionLabel && <Button disabled={disabled} small label={actionLabel} onClick={handleCancel} />}
       </div>
     </div>
   );
