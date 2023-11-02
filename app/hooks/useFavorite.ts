7e409e57b10329e5ed5805f393708b483fc61ac6
Eden Park <edengoforit@gmail.com>
2023-11-02 21:20:00 +1300

    usefavorite added

---
+++ app/hooks/useFavorite.ts
@@ -0,0 +1,50 @@
+import { useRouter } from "next/navigation";
+import { SafeUser } from "../types";
+import useLoginModal from "./useLoginModal";
+import { useCallback, useMemo } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+interface IUseFavorite {
+	listingId: string;
+	currentUser?: SafeUser | null
+}
+
+const useFavorite = ({ listingId, currentUser }: IUseFavorite) => {
+	const router = useRouter();
+	const loginModal = useLoginModal();
+
+	const hasFavorite = useMemo(() => {
+		const list = currentUser?.favoriteIds ?? [];
+
+		return list.includes(listingId);
+	}, [currentUser, listingId]);
+
+	const toggleFavorite = useCallback(async (e: React.MouseEvent<HTMLElement>) => {
+		e.stopPropagation();
+
+		if (!currentUser) {
+			return loginModal.onOpen();
+		}
+		try {
+			let request;
+			if (hasFavorite) {
+				request = () => axios.delete(`/api/favorites/${listingId}`);
+			} else {
+				request = () => axios.post(`/api/favorites/${listingId}`);
+			}
+			await request();
+
+			router.refresh();
+			toast.success('Success');
+		} catch (error) {
+			toast.error('Something went wrong');
+		}
+
+		return {
+			hasFavorite, toggleFavorite
+		};
+
+	}, [currentUser, hasFavorite, listingId, loginModal, router]);
+
+};
