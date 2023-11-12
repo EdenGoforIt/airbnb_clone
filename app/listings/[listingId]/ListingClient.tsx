b5d9f91f3a4a74b85bacb03fa770ff5ef8d43156
Eden Park <edengoforit@gmail.com>
2023-11-12 21:12:35 +1300

    list heading fix heart added

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -27,6 +27,7 @@ const ListClient: React.FC<ListingClientProps> = ({ reservations, listing, curre
             id={listing.id}
             currentUser={currentUser}
           />
+          <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6"></div>
         </div>
       </div>
     </Container>
