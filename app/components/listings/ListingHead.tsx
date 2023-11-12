8a2aa6f8efb8835d8b5ee34fa54e36134b5064f7
Eden Park <edengoforit@gmail.com>
2023-11-12 20:54:29 +1300

    listing head added

--- app/components/listings/ListingHead.tsx
+++ app/components/listings/ListingHead.tsx
@@ -1,5 +1,8 @@
+import useCountries from '@/app/hooks/useCountry';
 import { SafeUser } from '@/app/types';
-
+import Image from 'next/image';
+import Heading from '../Heading';
+import HeartButton from '../HeartButton';
 interface ListingHeadProps {
   id: string;
   locationValue: string;
@@ -9,7 +12,20 @@ interface ListingHeadProps {
 }
 
 const ListingHead: React.FC<ListingHeadProps> = ({ currentUser, locationValue, imageSrc, id, title }) => {
-  return <div></div>;
+  const { getByValue } = useCountries();
+  const location = getByValue(locationValue);
+
+  return (
+    <>
+      <Heading title={title} subtitle={`${location?.region}, ${location?.label}`} />
+      <div className="w-full h-[60vh] overflow-hidden rounded-xl relative">
+        <Image alt="image" src={imageSrc} fill className="object-cover w-full" />
+        <div className="absolute top-5 right-5">
+          <HeartButton listingId={id} currentUser={currentUser} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ListingHead;
