4e2d21bd034cb07cf7f4cc3cfbc6ca7940362c4c
Eden Park <edengoforit@gmail.com>
2023-12-17 21:28:22 +1300

    reservation route added

---
+++ app/api/reservations/route.ts
@@ -0,0 +1,42 @@
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import prisma from '@/app/libs/prismadb';
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+	const currentUser = await getCurrentUser();
+	console.log("🚀 ~ file: route.ts:7 ~ POST ~ currentUser:", currentUser)
+
+	if (!currentUser) {
+		return NextResponse.error();
+	}
+
+	const body = await request.json();
+	const {
+		listingId,
+		startDate,
+		endDate,
+		totalPrice
+	} = body;
+
+	if (!listingId || !startDate || !endDate || !totalPrice) {
+		return NextResponse.error();
+	}
+
+	const listingAndReservation = await prisma.listing.update({
+		where: {
+			id: listingId
+		},
+		data: {
+			reservations: {
+				create: {
+					userId: currentUser.id,
+					startDate,
+					endDate,
+					totalPrice
+				}
+			}
+		}
+	});
+
+	return NextResponse.json(listingAndReservation);
+}
